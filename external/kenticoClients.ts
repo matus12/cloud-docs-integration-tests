ea2d613451df80c4a6318ba1b97f5d808ba5d0a2
Juraj Bielik <jurajb@kentico.com>
2019-04-11 10:52:02 +0200

    KA-491 Setup custom retry policy for CM API SDK

--- external/kenticoClients.ts
+++ external/kenticoClients.ts
@@ -1,23 +1,18 @@
 import {
     ContentManagementClient,
     IContentManagementClient,
+    IContentManagementClientConfig,
 } from "kentico-cloud-content-management";
-import {
-    LIVE_API_KEY,
-    LIVE_PROJECT_ID,
-    TEST_API_KEY,
-    TEST_PROJECT_ID,
-} from "../shared/projectSettings";
+import { LIVE_API_KEY, LIVE_PROJECT_ID, TEST_API_KEY, TEST_PROJECT_ID } from "../shared/projectSettings";
 
 let testKenticoClient: IContentManagementClient;
 let liveKenticoClient: IContentManagementClient;
 
 export const getTestKenticoClient = () => {
     if (testKenticoClient === undefined) {
-        testKenticoClient = new ContentManagementClient({
-            apiKey: TEST_API_KEY,
-            projectId: TEST_PROJECT_ID,
-        });
+        testKenticoClient = new ContentManagementClient(
+            getContentManagementClientConfig(TEST_API_KEY, TEST_PROJECT_ID),
+        );
     }
 
     return testKenticoClient;
@@ -25,11 +20,17 @@ export const getTestKenticoClient = () => {
 
 export const getLiveKenticoClient = () => {
     if (liveKenticoClient === undefined) {
-        liveKenticoClient = new ContentManagementClient({
-            apiKey: LIVE_API_KEY,
-            projectId: LIVE_PROJECT_ID,
-        });
+        liveKenticoClient = new ContentManagementClient(
+            getContentManagementClientConfig(LIVE_API_KEY, LIVE_PROJECT_ID),
+        );
     }
 
     return liveKenticoClient;
 };
+
+const getContentManagementClientConfig = (apiKey: string, projectId: string): IContentManagementClientConfig => ({
+    apiKey,
+    projectId,
+    retryAttempts: 5,
+    retryStatusCodes: [500, 503],
+});
